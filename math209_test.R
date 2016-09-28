de5f58d6399e716aacc68a1850179f5055f12795
taylor <taylor@tarnold.local>
2016-09-28 18:11:33 -0400

    update scripts

--- math209_test.R
+++ math209_test.R
@@ -30,6 +30,38 @@ group_summarize <- function(.data, ...) {
   ungroup(results)
 }
 
+cm_basic <- function(data, formula, num_clusters = 2) {
+  out <- stats::lm(formula, data, method = "model.frame")
+  mm <- model.matrix(formula, data)
+  X <- scale(mm)
+
+  model <- flexclust::stepFlexclust(X, k = num_clusters, nrep = 10L, simple = TRUE)
+  model$call <- match.call()
+  model$terms <- out$terms
+  model$contrasts <- out$contrasts
+  model$xlevels <- out$xlevels
+  model$center <- attr(X,"scaled:center")
+  model$scale <- attr(X,"scaled:scale")
+  class(model) <- c("cm_basic", "kccasimple")
+
+  model
+}
+
+
+
+
+ca <- read_csv("https://statsmaths.github.io/stat_data/chicago_meta.csv")
+
+lm_basic(ca, owner_ratio ~ perc_20_units + median_income)
+lm_robust(ca, owner_ratio ~ perc_20_units + median_income)
+lm_select(ca, owner_ratio ~ perc_20_units + median_income)
+
+bm_basic(ca, owner_ratio > 43 ~ perc_20_units + median_income)
+z <- bm_robust(ca, owner_ratio > 43 ~ perc_20_units + median_income)
+bm_select(ca, owner_ratio > 43 ~ perc_20_units + median_income)
+
+
+
 
 
 
