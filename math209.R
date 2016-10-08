50bc95818c038552d04ff1131a9f022ca7a5eebc
taylor <taylor@tarnold.fios-router.home>
2016-10-08 11:22:35 -0400

    burrito

--- math209.R
+++ math209.R
@@ -9,6 +9,7 @@ check_and_load("dplyr")
 check_and_load("readr")
 check_and_load("ggrepel")
 check_and_load("robustbase")
+check_and_load("ggmap")
 
 # define a custom group_summary function
 group_summarize <- function(.data, ...) {
@@ -29,15 +30,8 @@ group_summarize <- function(.data, ...) {
 
 }
 
-lm_basic <- function(formula, data, ...) {
-  out <- stats::lm(formula, data)
-  out$call <- match.call()
-  class(out) = c("lm_basic", "lm")
-  out
-}
-
 lm_robust <- function(formula, data, ...) {
-  out <- robustbase::lmrob(formula, data, method = "MM")
+  out <- robustbase::lmrob(formula, data, setting = "KS2014")
   out$call <- match.call()
   class(out) = c("lm_robust", "lm")
   out
@@ -55,114 +49,12 @@ add_prediction <- function(data, object){
   data
 }
 
-summary.lm_basic <- function (object, correlation = FALSE, symbolic.cor = FALSE,
-    level = 0.95, mu = 0, ...)
-{
-    z <- object
-
-    # MODIFIED
-    ci_vals <- confint(object, level = level)
-
-    p <- z$rank
-    rdf <- z$df.residual
-    if (p == 0) {
-        r <- z$residuals
-        n <- length(r)
-        w <- z$weights
-        if (is.null(w)) {
-            rss <- sum(r^2)
-        }
-        else {
-            rss <- sum(w * r^2)
-            r <- sqrt(w) * r
-        }
-        resvar <- rss/rdf
-        ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
-        class(ans) <- "summary.lm"
-        ans$aliased <- is.na(coef(object))
-        ans$residuals <- r
-        ans$df <- c(0L, n, length(ans$aliased))
-        ans$coefficients <- matrix(NA, 0L, 4L)
-        dimnames(ans$coefficients) <- list(NULL, c("Estimate",
-            "Std. Error", "t value", "Pr(>|t|)"))
-        ans$sigma <- sqrt(resvar)
-        ans$r.squared <- ans$adj.r.squared <- 0
-        return(ans)
-    }
-    if (is.null(z$terms))
-        stop("invalid 'lm' object:  no 'terms' component")
-    if (!inherits(object, "lm"))
-        warning("calling summary.lm(<fake-lm-object>) ...")
-    Qr <- stats:::qr.lm(object)
-    n <- NROW(Qr$qr)
-    if (is.na(z$df.residual) || n - p != z$df.residual)
-        warning("residual degrees of freedom in object suggest this is not an \"lm\" fit")
-    r <- z$residuals
-    f <- z$fitted.values
-    w <- z$weights
-    if (is.null(w)) {
-        mss <- if (attr(z$terms, "intercept"))
-            sum((f - mean(f))^2)
-        else sum(f^2)
-        rss <- sum(r^2)
-    }
-    else {
-        mss <- if (attr(z$terms, "intercept")) {
-            m <- sum(w * f/sum(w))
-            sum(w * (f - m)^2)
-        }
-        else sum(w * f^2)
-        rss <- sum(w * r^2)
-        r <- sqrt(w) * r
-    }
-    resvar <- rss/rdf
-    if (is.finite(resvar) && resvar < (mean(f)^2 + var(f)) *
-        1e-30)
-        warning("essentially perfect fit: summary may be unreliable")
-    p1 <- 1L:p
-    R <- chol2inv(Qr$qr[p1, p1, drop = FALSE])
-    se <- sqrt(diag(R) * resvar)
-    est <- z$coefficients[Qr$pivot[p1]]
-    tval <- (est - mu)/se
-    ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
-    ans$residuals <- r
-    ans$coefficients <- cbind(est,ci_vals, 2 * pt(abs(tval),
-        rdf, lower.tail = FALSE))
-    dimnames(ans$coefficients) <- list(names(z$coefficients)[Qr$pivot[p1]],
-        c("Estimate", colnames(ci_vals), "Pr(>|t|)"))
-    ans$aliased <- is.na(coef(object))
-    ans$sigma <- sqrt(resvar)
-    ans$df <- c(p, rdf, NCOL(Qr$qr))
-    if (p != attr(z$terms, "intercept")) {
-        df.int <- if (attr(z$terms, "intercept"))
-            1L
-        else 0L
-        ans$r.squared <- mss/(mss + rss)
-        ans$adj.r.squared <- 1 - (1 - ans$r.squared) * ((n -
-            df.int)/rdf)
-        ans$fstatistic <- c(value = (mss/(p - df.int))/resvar,
-            numdf = p - df.int, dendf = rdf)
-    }
-    else ans$r.squared <- ans$adj.r.squared <- 0
-    ans$cov.unscaled <- R
-    dimnames(ans$cov.unscaled) <- dimnames(ans$coefficients)[c(1,
-        1)]
-    if (correlation) {
-        ans$correlation <- (R * resvar)/outer(se, se)
-        dimnames(ans$correlation) <- dimnames(ans$cov.unscaled)
-        ans$symbolic.cor <- symbolic.cor
-    }
-    if (!is.null(z$na.action))
-        ans$na.action <- z$na.action
-    class(ans) <- "summary.lm"
-    ans
-}
 
 summary.lm_robust <- function (object, correlation = FALSE, symbolic.cor = FALSE,
-    level = 0.95, mu = 0, ...)
+    conf.level = 0.95, mu = 0, ...)
 {
     # MODIFIED
-    ci_vals <- confint(object, level = level)
+    ci_vals <- confint(object, level = conf.level)
 
     if (is.null(object$terms))
         stop("invalid 'lmrob' object:  no terms component")
@@ -258,6 +150,10 @@ summary.lm_robust <- function (object, correlation = FALSE, symbolic.cor = FALSE
     if (is.function(ans$control$eps.x))
         ans$control$eps.x <- if (!is.null(o.x <- object[["x"]]))
             ans$control$eps.x(max(abs(o.x)))
+
+    # ADJUSTMENTS
+    #ans$coefficients[,4] <- p.adjust(ans$coefficients[,4])
+
     structure(ans, class = "summary.lm_robust")
 }
 
@@ -296,7 +192,8 @@ function (x, digits = max(3, getOption("digits") - 3), symbolic.cor = x$symbolic
                 else cat(sprintf("\nCoefficients of the %s-estimator:\n",
                   control$method))
             }
-            printCoefmat(x$coef, digits = digits, signif.stars = signif.stars,
+            printCoefmat(x$coef, digits = digits, signif.stars = FALSE,
+                has.Pvalue = TRUE,
                 ...)
         }
         else {
@@ -311,8 +208,8 @@ function (x, digits = max(3, getOption("digits") - 3), symbolic.cor = x$symbolic
                   colnames(coefs)))
                 coefs[!aliased, ] <- x$coefficients
             }
-            printCoefmat(coefs, digits = digits, signif.stars = signif.stars,
-                na.print = "NA", ...)
+            printCoefmat(coefs, digits = digits, signif.stars = FALSE,
+                na.print = "NA", has.Pvalue = TRUE, ...)
             cat("\nRobust residual standard error:", format(signif(x$scale,
                 digits)), "\n")
             if (!is.null(x$r.squared) && x$df[1] != attr(x$terms,
