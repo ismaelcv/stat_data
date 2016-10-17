46712d05de9d90443d0f09b676507db867160dd0
taylor <taylor@taylors-MacBook-Air.local>
2016-10-17 15:21:07 -0400

    update lm_basic function

--- math209.R
+++ math209.R
@@ -72,9 +72,9 @@ summary.lm_basic <- function (object, correlation = FALSE, symbolic.cor = FALSE,
         ans$aliased <- is.na(coef(object))
         ans$residuals <- r
         ans$df <- c(0L, n, length(ans$aliased))
-        ans$coefficients <- matrix(NA, 0L, 4L)
+        ans$coefficients <- matrix(NA, 0L, 3L)
         dimnames(ans$coefficients) <- list(NULL, c("Estimate",
-            "Std. Error", "t value", "Pr(>|t|)"))
+            "Std. Error", "t value"))
         ans$sigma <- sqrt(resvar)
         ans$r.squared <- ans$adj.r.squared <- 0
         return(ans)
@@ -116,10 +116,9 @@ summary.lm_basic <- function (object, correlation = FALSE, symbolic.cor = FALSE,
     tval <- (est - mu)/se
     ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
     ans$residuals <- r
-    ans$coefficients <- cbind(est,ci_vals, 2 * pt(abs(tval),
-        rdf, lower.tail = FALSE))
+    ans$coefficients <- cbind(est,ci_vals)
     dimnames(ans$coefficients) <- list(names(z$coefficients)[Qr$pivot[p1]],
-        c("Estimate", colnames(ci_vals), "Pr(>|t|)"))
+        c("Estimate", colnames(ci_vals)))
     ans$aliased <- is.na(coef(object))
     ans$sigma <- sqrt(resvar)
     ans$df <- c(p, rdf, NCOL(Qr$qr))
@@ -148,108 +147,108 @@ summary.lm_basic <- function (object, correlation = FALSE, symbolic.cor = FALSE,
     ans
 }
 
-summary.lm_robust <- function (object, correlation = FALSE, symbolic.cor = FALSE,
-    level = 0.95, mu = 0, ...)
-{
-    # MODIFIED
-    ci_vals <- confint(object, level = level)
+# summary.lm_robust <- function (object, correlation = FALSE, symbolic.cor = FALSE,
+#     level = 0.95, mu = 0, ...)
+# {
+#     # MODIFIED
+#     ci_vals <- confint(object, level = level)
 
-    if (is.null(object$terms))
-        stop("invalid 'lmrob' object:  no terms component")
-    p <- object$rank
-    df <- object$df.residual
-    sigma <- object[["scale"]]
-    aliased <- is.na(coef(object))
-    cf.nms <- c("Estimate", colnames(ci_vals), "Pr(>|t|)")
-    if (p > 0) {
-        n <- p + df
-        p1 <- seq_len(p)
-        se <- sqrt(if (length(object$cov) == 1L) object$cov else diag(object$cov))
-        est <- object$coefficients[object$qr$pivot[p1]]
-        tval <- (est - mu)/se
-        ans <- object[c("call", "terms", "residuals", "scale",
-            "rweights", "converged", "iter", "control")]
-        if (!is.null(ans$weights))
-            ans$residuals <- ans$residuals * sqrt(object$weights)
-        ans$df <- c(p, df, NCOL(object$qr$qr))
-        ans$coefficients <- if (ans$converged)
-            cbind(est, ci_vals, 2 * pt(abs(tval), df, lower.tail = FALSE))
-        else cbind(est, if (sigma <= 0)
-            0
-        else NA, NA, NA)
-        dimnames(ans$coefficients) <- list(names(est), cf.nms)
-        if (p != attr(ans$terms, "intercept")) {
-            df.int <- if (attr(ans$terms, "intercept"))
-                1L
-            else 0L
-            resid <- object$residuals
-            pred <- object$fitted.values
-            resp <- if (is.null(object[["y"]]))
-                pred + resid
-            else object$y
-            wgt <- object$rweights
-            ctrl <- object$control
-            c.psi <- ctrl$tuning.psi
-            psi <- ctrl$psi
-            correc <- if (psi == "ggw") {
-                if (isTRUE(all.equal(c.psi, c(-0.5, 1, 0.95,
-                  NA))))
-                  1.121708
-                else if (isTRUE(all.equal(c.psi, c(-0.5, 1.5,
-                  0.95, NA))))
-                  1.163192
-                else if (isTRUE(all.equal(c.psi, c(-0.5, 1, 0.85,
-                  NA))))
-                  1.33517
-                else if (isTRUE(all.equal(c.psi, c(-0.5, 1.5,
-                  0.85, NA))))
-                  1.395828
-                else lmrob.E(wgt(r), ctrl)/lmrob.E(r * psi(r),
-                  ctrl)
-            }
-            else if (any(psi == robustbase:::.Mpsi.R.names) && isTRUE(all.equal(c.psi,
-                .Mpsi.tuning.default(psi)))) {
-                switch(psi, bisquare = 1.207617, welsh = 1.224617,
-                  optimal = 1.068939, hampel = 1.166891, lqq = 1.159232,
-                  stop("unsupported psi function -- should not happen"))
-            }
-            else lmrob.E(wgt(r), ctrl)/lmrob.E(r * psi(r), ctrl)
-            resp.mean <- if (df.int == 1L)
-                sum(wgt * resp)/sum(wgt)
-            else 0
-            yMy <- sum(wgt * (resp - resp.mean)^2)
-            rMr <- sum(wgt * resid^2)
-            ans$r.squared <- r2correc <- (yMy - rMr)/(yMy + rMr *
-                (correc - 1))
-            ans$adj.r.squared <- 1 - (1 - r2correc) * ((n - df.int)/df)
-        }
-        else ans$r.squared <- ans$adj.r.squared <- 0
-        ans$cov <- object$cov
-        if (length(object$cov) > 1L)
-            dimnames(ans$cov) <- dimnames(ans$coefficients)[c(1,
-                1)]
-        if (correlation) {
-            ans$correlation <- ans$cov/outer(se, se)
-            ans$symbolic.cor <- symbolic.cor
-        }
-    }
-    else {
-        ans <- object
-        ans$df <- c(0L, df, length(aliased))
-        ans$coefficients <- matrix(NA, 0L, 4L, dimnames = list(NULL,
-            cf.nms))
-        ans$r.squared <- ans$adj.r.squared <- 0
-        ans$cov <- object$cov
-    }
-    ans$aliased <- aliased
-    ans$sigma <- sigma
-    if (is.function(ans$control$eps.outlier))
-        ans$control$eps.outlier <- ans$control$eps.outlier(nobs(object))
-    if (is.function(ans$control$eps.x))
-        ans$control$eps.x <- if (!is.null(o.x <- object[["x"]]))
-            ans$control$eps.x(max(abs(o.x)))
-    structure(ans, class = "summary.lm_robust")
-}
+#     if (is.null(object$terms))
+#         stop("invalid 'lmrob' object:  no terms component")
+#     p <- object$rank
+#     df <- object$df.residual
+#     sigma <- object[["scale"]]
+#     aliased <- is.na(coef(object))
+#     cf.nms <- c("Estimate", colnames(ci_vals), "Pr(>|t|)")
+#     if (p > 0) {
+#         n <- p + df
+#         p1 <- seq_len(p)
+#         se <- sqrt(if (length(object$cov) == 1L) object$cov else diag(object$cov))
+#         est <- object$coefficients[object$qr$pivot[p1]]
+#         tval <- (est - mu)/se
+#         ans <- object[c("call", "terms", "residuals", "scale",
+#             "rweights", "converged", "iter", "control")]
+#         if (!is.null(ans$weights))
+#             ans$residuals <- ans$residuals * sqrt(object$weights)
+#         ans$df <- c(p, df, NCOL(object$qr$qr))
+#         ans$coefficients <- if (ans$converged)
+#             cbind(est, ci_vals, 2 * pt(abs(tval), df, lower.tail = FALSE))
+#         else cbind(est, if (sigma <= 0)
+#             0
+#         else NA, NA, NA)
+#         dimnames(ans$coefficients) <- list(names(est), cf.nms)
+#         if (p != attr(ans$terms, "intercept")) {
+#             df.int <- if (attr(ans$terms, "intercept"))
+#                 1L
+#             else 0L
+#             resid <- object$residuals
+#             pred <- object$fitted.values
+#             resp <- if (is.null(object[["y"]]))
+#                 pred + resid
+#             else object$y
+#             wgt <- object$rweights
+#             ctrl <- object$control
+#             c.psi <- ctrl$tuning.psi
+#             psi <- ctrl$psi
+#             correc <- if (psi == "ggw") {
+#                 if (isTRUE(all.equal(c.psi, c(-0.5, 1, 0.95,
+#                   NA))))
+#                   1.121708
+#                 else if (isTRUE(all.equal(c.psi, c(-0.5, 1.5,
+#                   0.95, NA))))
+#                   1.163192
+#                 else if (isTRUE(all.equal(c.psi, c(-0.5, 1, 0.85,
+#                   NA))))
+#                   1.33517
+#                 else if (isTRUE(all.equal(c.psi, c(-0.5, 1.5,
+#                   0.85, NA))))
+#                   1.395828
+#                 else lmrob.E(wgt(r), ctrl)/lmrob.E(r * psi(r),
+#                   ctrl)
+#             }
+#             else if (any(psi == robustbase:::.Mpsi.R.names) && isTRUE(all.equal(c.psi,
+#                 .Mpsi.tuning.default(psi)))) {
+#                 switch(psi, bisquare = 1.207617, welsh = 1.224617,
+#                   optimal = 1.068939, hampel = 1.166891, lqq = 1.159232,
+#                   stop("unsupported psi function -- should not happen"))
+#             }
+#             else lmrob.E(wgt(r), ctrl)/lmrob.E(r * psi(r), ctrl)
+#             resp.mean <- if (df.int == 1L)
+#                 sum(wgt * resp)/sum(wgt)
+#             else 0
+#             yMy <- sum(wgt * (resp - resp.mean)^2)
+#             rMr <- sum(wgt * resid^2)
+#             ans$r.squared <- r2correc <- (yMy - rMr)/(yMy + rMr *
+#                 (correc - 1))
+#             ans$adj.r.squared <- 1 - (1 - r2correc) * ((n - df.int)/df)
+#         }
+#         else ans$r.squared <- ans$adj.r.squared <- 0
+#         ans$cov <- object$cov
+#         if (length(object$cov) > 1L)
+#             dimnames(ans$cov) <- dimnames(ans$coefficients)[c(1,
+#                 1)]
+#         if (correlation) {
+#             ans$correlation <- ans$cov/outer(se, se)
+#             ans$symbolic.cor <- symbolic.cor
+#         }
+#     }
+#     else {
+#         ans <- object
+#         ans$df <- c(0L, df, length(aliased))
+#         ans$coefficients <- matrix(NA, 0L, 4L, dimnames = list(NULL,
+#             cf.nms))
+#         ans$r.squared <- ans$adj.r.squared <- 0
+#         ans$cov <- object$cov
+#     }
+#     ans$aliased <- aliased
+#     ans$sigma <- sigma
+#     if (is.function(ans$control$eps.outlier))
+#         ans$control$eps.outlier <- ans$control$eps.outlier(nobs(object))
+#     if (is.function(ans$control$eps.x))
+#         ans$control$eps.x <- if (!is.null(o.x <- object[["x"]]))
+#             ans$control$eps.x(max(abs(o.x)))
+#     structure(ans, class = "summary.lm_robust")
+# }
 
 add_prediction <- function(data, object){
   pred <- predict(object, newdata = data)
